3b3181fa00042d7f2b6d5807b87f31d711610141
SohailK <sohail.kumar03@gmail.com>
2025-03-12 22:20:30 -0400

    minor testing changes

--- Main.cpp
+++ Main.cpp
@@ -77,18 +77,18 @@ int main(int argc, char* argv[]) {
     //Checkerboard ground
 	glm::vec3 color1 = glm::vec3(1.0f, 0.2f, 0.6f);
     glm::vec3 color2 = glm::vec3(0.6f, 0.0f, 0.6f);
-    world.Add(std::make_unique<Triangle>(Triangle({ corner1, corner2, corner3 }, std::make_unique<Mat_Checkerboard>(color1, color2, 0.1, 0.1))));
-    world.Add(std::make_unique<Triangle>(Triangle({ corner3, corner2, corner4 }, std::make_unique<Mat_Checkerboard>(color1, color2, 0.1, 0.1))));
+    world.Add(std::make_unique<Triangle>(Triangle({ corner1, corner2, corner3 }, std::make_unique<Mat_Checkerboard>(color1, color2, 0.1, 0.25))));
+    world.Add(std::make_unique<Triangle>(Triangle({ corner3, corner2, corner4 }, std::make_unique<Mat_Checkerboard>(color1, color2, 0.1, 0.25))));
 
     //Phong Shaded ground
-    //world.Add(std::make_unique<Triangle>(Triangle({ corner1, corner2, corner3 }, std::make_unique<Mat_Phong>(glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), 0.5f, 0.5f, 1.0f, 2.0f))));
-    //world.Add(std::make_unique<Triangle>(Triangle({ corner3, corner2, corner4 }, std::make_unique<Mat_Phong>(glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), 0.5f, 0.5f, 1.0f, 2.0f))));
+    //world.Add(std::make_unique<Triangle>(Triangle({ corner1, corner2, corner3 }, std::make_unique<Mat_Phong>(glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), 1.0f, 0.5f, 1.0f, 2.0f))));
+    //world.Add(std::make_unique<Triangle>(Triangle({ corner3, corner2, corner4 }, std::make_unique<Mat_Phong>(glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), 1.0f, 0.5f, 1.0f, 2.0f))));
 
 
     // lights
-    world.Add(std::make_unique<Light>(Light(glm::vec3(1.5f, 10.0f, 1.0f), glm::vec3(0.7f, 0.7f, 0.7f))));
-    //world.Add(std::make_unique<Light>(Light(glm::vec3(1.5f, 10.0f, 1.0f), glm::vec3(0.3f, 0.3f, 0.3f))));
-    //world.Add(std::make_unique<Light>(Light(glm::vec3(0.0f, 8.0f, 4.0f), glm::vec3(0.2f, 0.2f, 0.2f))));
+    world.Add(std::make_unique<Light>(Light(glm::vec3(1.5f, 10.0f, 1.0f), glm::vec3(0.5f, 0.5f, 0.5f))));
+    //world.Add(std::make_unique<Light>(Light(glm::vec3(1.5f, 10.0f, 1.0f), glm::vec3(0.5f, 0.5f, 0.5f))));
+    //world.Add(std::make_unique<Light>(Light(glm::vec3(0.0f, 8.0f, 4.0f), glm::vec3(0.4f, 0.4f, 0.4f))));
 
 
     //Add camera
